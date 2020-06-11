425ac26513a690ce1cf231fc5637cc76d531852a
Daeki <kimdaeki@gmail.com>
2020-06-11 16:17:24 +0900

    sadsa

--- src/main/java/com/iu/s1/board/BoardVO.java
+++ src/main/java/com/iu/s1/board/BoardVO.java
@@ -22,7 +22,7 @@ public class BoardVO {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long num;
+	private Long num;
 	@Column
 	private String title;
 	@Column
@@ -34,6 +34,6 @@ public class BoardVO {
 	//@UpdateTimestamp
 	private Date regDate;
 	@Column
-	private long hit;
+	private Long hit;
 
 }
--- src/main/java/com/iu/s1/board/qna/QnaService.java
+++ src/main/java/com/iu/s1/board/qna/QnaService.java
@@ -1,11 +1,14 @@
 package com.iu.s1.board.qna;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional(rollbackOn = Exception.class)
 public class QnaService {
 	@Autowired
 	private QnaRepository qnaRepository;
@@ -18,6 +21,9 @@ public class QnaService {
 		//원본글 
 		//ref = 자기자신의 글번호
 		//step, depth 0
+		qnaVO.setRef(0);
+		qnaVO.setStep(0);
+		qnaVO.setDepth(0);
 		qnaVO=qnaRepository.save(qnaVO);
 		qnaVO.setRef(qnaVO.getNum());
 		return qnaRepository.save(qnaVO);
